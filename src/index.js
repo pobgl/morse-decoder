556a81a15afb261904e8d91709638ae40abe0bd5
Gleb Pobezhimov <pglb@yandex.ru>
2020-10-26 00:55:00 +0400

    feat: add morse-decoder solution;

--- src/index.js
+++ src/index.js
@@ -39,6 +39,25 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    
+    let decoded = "";
+    for (let i = 0; i < expr.length; i += 10) {
+        let morse = expr.slice(i, i + 10);
+        if (morse === "**********") {
+            decoded += " ";
+            continue;
+        }
+        let decodeChar = "";
+        for (let n = 0; n < morse.length; n += 2) {
+            let str = morse.slice(n, n + 2);
+
+            if (str != "00") {
+                decodeChar += str == "10" ? "." : "-";
+            }
+        }
+        decoded += MORSE_TABLE[decodeChar];
+    }
+    return decoded;
 }
 
 module.exports = {
